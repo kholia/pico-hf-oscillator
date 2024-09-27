aa61c18f31710dd19365f36dcd709f7d7a5c6959
Dhiru Kholia <dhiru.kholia@elastic.co>
2024-09-27 20:45:39 +0530

    Support for Pico 2 + CW beacon

--- test.c
+++ test.c
@@ -7,13 +7,13 @@
 //
 //
 //  test.c - Simple tests of digital controlled radio freq oscillator.
-// 
+//
 //
 //  DESCRIPTION
 //
 //      The oscillator provides precise generation of any frequency ranging
 //  from 1 Hz to 33.333 MHz with tenth's of millihertz resolution (please note that
-//  this is relative resolution owing to the fact that the absolute accuracy of 
+//  this is relative resolution owing to the fact that the absolute accuracy of
 //  onboard crystal of pi pico is limited; the absoulte accuracy can be provided
 //  when using GPS reference option included).
 //      The DCO uses phase locked loop principle programmed in C and PIO asm.
@@ -45,7 +45,7 @@
 //      Raspberry Pi pico.
 //
 //  REVISION HISTORY
-// 
+//
 //      Rev 0.1   05 Nov 2023   Initial release
 //      Rev 0.2   18 Nov 2023
 //      Rev 1.0   10 Dec 2023   Improved frequency range (to ~33.333 MHz).
@@ -57,7 +57,7 @@
 //      MIT License (http://www.opensource.org/licenses/mit-license.php)
 //
 //  Copyright (c) 2023 by Roman Piksaykin
-//  
+//
 //  Permission is hereby granted, free of charge,to any person obtaining a copy
 //  of this software and associated documentation files (the Software), to deal
 //  in the Software without restriction,including without limitation the rights
@@ -77,14 +77,16 @@
 //  THE SOFTWARE.
 ///////////////////////////////////////////////////////////////////////////////
 #include <string.h>
-#include <stdlib.h>
+#include "pico/stdlib.h"
 #include <stdio.h>
 
 #include "defines.h"
+#include "pico.h"
 
 #include "piodco/piodco.h"
-#include "build/dco2.pio.h"
+#include "dco2.pio.h"
 #include "hardware/vreg.h"
+#include "hardware/clocks.h"
 #include "pico/multicore.h"
 #include "pico/stdio/driver.h"
 
@@ -99,215 +101,134 @@
 #include "protos.h"
 
 //#define GEN_FRQ_HZ 32333333L
-#define GEN_FRQ_HZ 29977777L
+#define GEN_FRQ_HZ 28023000L
 
 PioDco DCO; /* External in order to access in both cores. */
 
-int main() 
-{
-    const uint32_t clkhz = PLL_SYS_MHZ * 1000000L;
-    set_sys_clock_khz(clkhz / 1000L, true);
+// https://www.codebug.org.uk/learn/step/541/morse-code-timing-rules/
+const uint DOT_PERIOD_MS = 92;  // 13 WPM
 
-    stdio_init_all();
+const char *morse_letters[] = {
+  ".-",    // A
+  "-...",  // B
+  "-.-.",  // C
+  "-..",   // D
+  ".",     // E
+  "..-.",  // F
+  "--.",   // G
+  "....",  // H
+  "..",    // I
+  ".---",  // J
+  "-.-",   // K
+  ".-..",  // L
+  "--",    // M
+  "-.",    // N
+  "---",   // O
+  ".--.",  // P
+  "--.-",  // Q
+  ".-.",   // R
+  "...",   // S
+  "-",     // T
+  "..-",   // U
+  "...-",  // V
+  ".--",   // W
+  "-..-",  // X
+  "-.--",  // Y
+  "--.."   // Z
+};
+
+const char *morse_numbers[] = {
+  "-----",  // 0
+  ".----",  // 1
+  "..---",  // 2
+  "...--",  // 3
+  "....-",  // 4
+  ".....",  // 5
+  "-....",  // 6
+  "--...",  // 7
+  "---..",  // 8
+  "----."   // 9
+};
+
+void on() {
+  PioDCOStart(&DCO);
+}
+
+void off() {
+  PioDCOStop(&DCO);
+  gpio_init(DCO._gpio);
+  pio_gpio_init(DCO._pio, DCO._gpio);
+}
+
+void put_morse_letter(const char *pattern) {
+  puts(pattern);
+  for (; *pattern; ++pattern) {
+    on();
+    if (*pattern == '.')
+      sleep_ms(DOT_PERIOD_MS);
+    else
+      sleep_ms(DOT_PERIOD_MS * 3);
+    off();
+    sleep_ms(DOT_PERIOD_MS * 1);
+  }
+  sleep_ms(DOT_PERIOD_MS * 2);
+}
+
+void put_morse_str(const char *str) {
+  for (; *str; ++str) {
+    printf("%c\n", *str);
+    if (*str >= 'A' && *str <= 'Z') {
+      put_morse_letter(morse_letters[*str - 'A']);
+    } else if (*str >= 'a' && *str <= 'z') {
+      put_morse_letter(morse_letters[*str - 'a']);
+    } else if (*str >= '0' && *str <= '9') {
+      put_morse_letter(morse_numbers[*str - '0']);
+    } else if (*str == '?') {
+      put_morse_letter("..--..");  // k1te ? Question mark
+    } else if (*str == '/') {
+      put_morse_letter("-..-.");  // k1te ~ for /B
+    } else if (*str == '~') {
+      put_morse_letter("..--");  // k1te ~ THE 'pi' SYMBOL (new 10/01/2022)
+    } else if (*str == ' ') {
+      sleep_ms(DOT_PERIOD_MS * 4);
+    }
+  }
+}
+
+int main() {
+  const uint32_t clkhz = PLL_SYS_MHZ * 1000000L;
+  set_sys_clock_khz(clkhz / 1000L, true);
+
+  stdio_init_all();
+
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+  multicore_launch_core1(core1_entry);
+
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+  while (1) {
+    put_morse_str("CQ CQ CQ DE VU3CER TEST BEACON");
     sleep_ms(1000);
-    printf("Start\n");
-
-    HFconsoleContext *phfc = HFconsoleInit(-1, 0);
-    HFconsoleSetWrapper(phfc, ConsoleCommandsWrapper);
-
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-
-    multicore_launch_core1(core1_entry);
-
-    for(;;)
-    {
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(5);
-        int r = HFconsoleProcess(phfc, 10);
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(1);
-    }
-
-    for(;;)
-    {
-        sleep_ms(100);
-        int chr = getchar_timeout_us(100);//getchar();
-        printf("%d %c\n", chr, (char)chr);
-    }
-  
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-
-    multicore_launch_core1(core1_entry);
-
-    //SpinnerDummyTest();
-    //SpinnerSweepTest();
-    //SpinnerMFSKTest();
-    SpinnerRTTYTest();
-    //SpinnerMilliHertzTest();
-    //SpinnerWide4FSKTest();
-    //SpinnerGPSreferenceTest();
+  }
 }
 
-/* This is the code of dedicated core. 
+/* This is the code of dedicated core.
    We deal with extremely precise real-time task. */
-void core1_entry()
-{
-    const uint32_t clkhz = PLL_SYS_MHZ * 1000000L;
+void core1_entry() {
+  const uint32_t clkhz = PLL_SYS_MHZ * 1000000L;
 
-    /* Initialize DCO */
-    assert_(0 == PioDCOInit(&DCO, 6, clkhz));
+  /* Initialize DCO */
+  assert_(0 == PioDCOInit(&DCO, 6, clkhz));
 
-    /* Run DCO. */
-    PioDCOStart(&DCO);
+  /* Run DCO. */
+  PioDCOStart(&DCO);
 
-    /* Set initial freq. */
-    assert_(0 == PioDCOSetFreq(&DCO, GEN_FRQ_HZ, 0u));
+  /* Set initial freq. */
+  assert_(0 == PioDCOSetFreq(&DCO, GEN_FRQ_HZ, 0u));
 
-    /* Run the main DCO algorithm. It spins forever. */
-    PioDCOWorker2(&DCO);
-}
-
-void RAM (SpinnerMFSKTest)(void)
-{
-    uint32_t rndval = 77777777;
-    for(;;)
-    {
-        /* This example sets new RND frequency every ~250 ms.
-           Frequency shift is 5 Hz for each step.
-        */
-        PioDCOSetFreq(&DCO, GEN_FRQ_HZ - 5*(rndval & 7), 0u);
-
-        /* LED signal */
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(250);
-
-        PRN32(&rndval);
-    }
-}
-
-void RAM (SpinnerSweepTest)(void)
-{
-    int i = 0;
-    for(;;)
-    {
-        /* This example sets new frequency every ~250 ms.
-           Frequency shift is 5 Hz for each step.
-        */
-        PioDCOSetFreq(&DCO, GEN_FRQ_HZ - 5*i, 0u);
-
-        /* LED signal */
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(500);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(500);
-
-        /* Return to initial freq after 20 steps (100 Hz). */
-        if(++i == 20)
-            i = 0;
-    }
-}
-
-void RAM (SpinnerRTTYTest)(void)
-{
-    int32_t df = 170;   /* 170 Hz freq diff. */
-    uint32_t rndval = 77777777;
-    for(;;)
-    {
-        /* This example sets new PRN frequency every ~22 ms.
-           Note: You should use precise timing while building a real transmitter.
-        */
-        PioDCOSetFreq(&DCO, GEN_FRQ_HZ - df*(rndval & 1), 0u);
-
-        /* LED signal */
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(22);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(22);
-
-        PRN32(&rndval);
-    }
-}
-
-void RAM (SpinnerMilliHertzTest)(void)
-{
-    int i = 0;
-    for(;;)
-    {
-        /* This example sets new frequency every ~1s.
-           Frequency shift is 0.99 Hz for each step.
-        */
-        PioDCOSetFreq(&DCO, GEN_FRQ_HZ, 990*(++i&1));
-
-        /* LED signal */
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(1000);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(1000);
-    }
-}
-
-void RAM (SpinnerWide4FSKTest)(void)
-{
-    int32_t df = 100;   /* 100 Hz freq diff * 4 = 400 Hz. */
-    uint32_t rndval = 77777777;
-    for(;;)
-    {
-        /* This example sets new PRN frequency every ~20 ms.
-           Note: You should use precise timing while building a real transmitter.
-        */
-        PioDCOSetFreq(&DCO, GEN_FRQ_HZ - df*(rndval & 3), 0u);
-
-        /* LED signal */
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(20);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(20);
-
-        PRN32(&rndval);
-    }
-}
-
-/* This example sets the OUT frequency to 5.555 MHz.
-   Next every ~1 sec the shift of the OUT frequency relative to GPS
-   reference is calculated and the OUT frequency is corrected.
-   The example is working only when GPS receiver provides an
-   accurate PPS output (pulse per second). If no such option,
-   the correction parameter is zero.
-*/
-void RAM (SpinnerGPSreferenceTest)(void)
-{
-    const uint32_t ku32_freq = 5555000UL;
-    const int kigps_pps_pin = 2;
-
-    int32_t i32_compensation_millis = 0;
-
-    GPStimeContext *pGPS = GPStimeInit(0, 9600, kigps_pps_pin);
-    assert_(pGPS);
-    DCO._pGPStime = pGPS;
-    int tick = 0;
-    for(;;)
-    {
-        PioDCOSetFreq(&DCO, ku32_freq, -2*i32_compensation_millis);
-
-        /* LED signal */
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(2500);
-
-        i32_compensation_millis = 
-            PioDCOGetFreqShiftMilliHertz(&DCO, (uint64_t)(ku32_freq * 1000LL));
-
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(2500);
-
-        if(0 == ++tick % 6)
-        {
-            //stdio_set_driver_enabled(&stdio_uart, false);
-            GPStimeDump(&(pGPS->_time_data));
-            //stdio_set_driver_enabled(&stdio_uart, true);
-        }
-    }
+  /* Run the main DCO algorithm. It spins forever. */
+  PioDCOWorker2(&DCO);
 }
